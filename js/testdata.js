e1953c1ed1f4e557b1d9ddfe27d674835c29cac8
Thirosh Madhusha <thiroshmadhusha0520@gmail.com>
2025-10-05 17:52:10 +0530

    Create testdata.js

---
+++ js/testdata.js
@@ -0,0 +1,213 @@
+const postsData = [
+    // Beach
+
+    {
+        img: '/assets/beachs/goyambokka2.jpg',
+        category: 'Beach',
+        title: 'Goyambokka Beach',
+        description: 'Lorem',
+    },
+    {
+        img: '/assets/beachs/silent1.jpeg',
+        category: 'Beach',
+        title: 'Silent Beach',
+        description: 'Lorem',
+    },
+
+    {
+        img: '/assets/beachs/redbeach.jpg',
+        category: 'Beach',
+        title: 'Red Beach',
+        description: 'Lorem',
+    },
+    {
+        img: '/assets/beachs/hiriketiya.jpg',
+        category: 'Beach',
+        title: 'Hiriketiya Beach',
+        description: 'Lorem',
+    },
+    {
+        img: '/assets/beachs/paraviwalla.jpg',
+        category: 'Beach',
+        title: 'Tangalle Paravi Wella Beach',
+        description: 'Lorem',
+    },
+ 
+    {
+        img: '/assets/beachs/medilla.jpg',
+        category: 'Beach',
+        title: 'Madaketiya & Medilla Beach',
+        description: 'Lorem',
+    },
+    {
+        img: '/assets/beachs/mirissa.jpg',
+        category: 'Beach',
+        title: 'Mirissa Beach',
+        description: 'Lorem',
+    },
+    {
+        img: '/assets/beachs/unawatuna.jpg',
+        category: 'Beach',
+        title: 'Unawatuna Beach',
+        description: 'Lorem',
+    },
+
+    // Popular
+
+    {
+        img: '/assets/popular/birds.jpg',
+        category: 'Popular',
+        title: 'Birds Park in Hambantota',
+        description: 'Lorem',
+    },
+    {
+        img: '/assets/temple/mulkirigala.jpeg',
+        category: 'Popular',
+        title: 'Mulkirigala Rock Monastery',
+        description: 'Lorem',
+    },
+
+    {
+        img: '/assets/popular/gallfort.jpg',
+        category: 'Popular',
+        title: 'Galle Fort',
+        description: 'Lorem',
+    },
+    {
+        img: '/assets/popular/thalawakale.jpg',
+        category: 'Popular',
+        title: 'Talawakelle Tea Factory',
+        description: 'Lorem',
+    },
+
+    // Natural
+
+    {
+        img: '/assets/animals/yala.jpg',
+        category: 'Nature',
+        title: 'Yala National Park',
+        description: 'Lorem',
+    },
+    {
+        img: '/assets/nature/blowhole.jpg',
+        category: 'Nature',
+        title: 'Hummanaya Blow Hole',
+        description: 'Lorem',
+    },
+    {
+        img: '/assets/animals/turtle.jpeg',
+        category: 'Nature',
+        title: 'Turtle Watch Rekawa',
+        description: 'Lorem',
+    },
+    {
+        img: '/assets/animals/udawalawa.jpg',
+        category: 'Nature',
+        title: 'Uda Walawe National Park',
+        description: 'Lorem',
+    },
+    {
+        img: '/assets/nature/tangallelagoon3.jpg',
+        category: 'Nature',
+        title: 'Tangalle Lagoon - Kayak',
+        description: 'Lorem',
+    },
+    {
+        img: '/assets/animals/whale.jpg',
+        category: 'Nature',
+        title: 'Whale Watching',
+        description: 'Lorem',
+    },
+    {
+        img: '/assets/nature/sinharaja.jpeg',
+        category: 'Nature',
+        title: 'Sinharaja Rain Forest',
+        description: 'Lorem',
+    },
+    {
+        img: '/assets/nature/bundala.jpg',
+        category: 'Nature',
+        title: 'Bundala National Park',
+        description: 'Lorem',
+    },
+    {
+        img: '/assets/animals/kalamatiya.jpg',
+        category: 'Nature',
+        title: 'Kalametiya Bird Sanctuary',
+        description: 'Lorem',
+    },
+    {
+        img: '/assets/animals/turtle2.jpg',
+        category: 'Nature',
+        title: 'Batheegama Turtle Point',
+        description: 'Lorem',
+    },
+    {
+        img: '/assets/nature/rekawalagoon1.jpeg',
+        category: 'Nature',
+        title: 'Rekawa Lagoon',
+        description: 'Lorem',
+    },
+    {
+        img: '/assets/nature/kumana.jpg',
+        category: 'Nature',
+        title: 'Kumana National Park',
+        description: 'Lorem',
+    },
+
+    // Cultural
+
+    {
+        img: '/assets/popular/lighthouse.jpg',
+        category: 'Popular',
+        title: 'Dondra Head Lighthouse',
+        description: 'Lorem',
+    },
+    {
+        img: '/assets/popular/tea.jpg',
+        category: 'Cultural',
+        title: 'Handunugoda Tea Estate',
+        description: 'Lorem',
+    },
+    {
+        img: '/assets/popular/mask.jpg',
+        category: 'Cultural',
+        title: 'Ambalangoda Mask Museum',
+        description: 'Lorem',
+    },
+    {
+        img: '/assets/temple/waurukannala.jpg',
+        category: 'Cultural',
+        title: 'Wevurukannala Vihara Temple',
+        description: 'Lorem',
+    },
+    // Popular
+
+
+
+
+
+
+    // Natural
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // Cultural
+
+
+
+
+
+
+];
