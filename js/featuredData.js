e9e3b5bbbd6785000e9ef28640428abc639b6e8b
Thirosh Madhusha <thiroshmadhusha0520@gmail.com>
2025-10-05 17:49:25 +0530

    Create featuredData.js

---
+++ js/featuredData.js
@@ -0,0 +1,39 @@
+// js/featuredData.js
+window.featuredPostsData = [
+  {
+    img: '/assets/nature/blebeach.jpg',
+    title: 'Yala National Park',
+    location: 'Monaragala',
+    price: '20',
+    priceUnit: '/per tour',
+    rating: '4.6',
+    reviews: '1'
+  },
+  {
+    img: '/assets/beachs/goyambokka2.jpg',
+    title: 'Arugam Bay',
+    location: 'Tangalle',
+    price: '99',
+    priceUnit: '/per tour',
+    rating: '4.8',
+    reviews: '2'
+  },
+  {
+    img: '/assets/nature/tangallelagoon3.jpg',
+    title: 'Whale Watching',
+    location: 'Mirissa',
+    price: '20',
+    priceUnit: '/per tour',
+    rating: '4.6',
+    reviews: '1'
+  },
+  {
+    img: '/assets/popular/lighthouse.jpg',
+    title: 'Udawalawe National Park',
+    location: 'Udawalawe',
+    price: '80',
+    priceUnit: '/per tour',
+    rating: '4.6',
+    reviews: '1'
+  }
+];
